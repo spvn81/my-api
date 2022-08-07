ec5dabd33c65167aebfde398357c7c8e958438a5
spvn81 <spvn81@gmail.com>
2022-08-07 21:40:03 +0530

    chnage sadded

--- database/seeders/userData.php
+++ database/seeders/userData.php
@@ -3,16 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use \app\Models\User;
 
-class DatabaseSeeder extends Seeder
+class userData extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->times(40)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('/users', function () {
+    return UserResource::collection(User::all());
 });
